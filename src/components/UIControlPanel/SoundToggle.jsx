5afdbc33b8cf903d009bcb72163ab1a4b3eb4f65
Jessica Ragnar <Jessica.e.ragnar@gmail.com>
2024-05-27 11:20:52 +0200

    stuff

--- src/components/UIControlPanel/SoundToggle.jsx
+++ src/components/UIControlPanel/SoundToggle.jsx
@@ -5,7 +5,6 @@ import music from '../../assets/music/SPRING OF DECEPTION - Density & Time.mp3';
 const SoundToggle = () => {
     const audioRef = useRef(new Audio(music)); 
     const [soundOn, setSoundOn] = useState(() => {
-
         const savedSoundOn = sessionStorage.getItem('soundOn');
         return savedSoundOn === null ? true : JSON.parse(savedSoundOn);
     });
@@ -38,6 +37,21 @@ const SoundToggle = () => {
             audioRef.current.pause();
         };
     }, [soundOn]);
+
+    useEffect(() => {
+        const handleAutoplay = async () => {
+            if (soundOn) {
+                try {
+                    await audioRef.current.play();
+                } catch (error) {
+                    console.error("Autoplay error:", error);
+                    // You can prompt the user to interact with the page here
+                }
+            }
+        };
+
+        handleAutoplay();
+    }, []);
 
     return (
         <div className="bg-cardbg/80 p-3 rounded-l-lg cursor-pointer hover:bg-nightblue/80 transition-colors"
